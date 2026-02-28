c07d3f7e135047248fac36434b1055721f99da0f
Abhi <abhilakshyag@gmail.com>
2026-02-28 19:26:56 +0530

    Added tasks page route to server and set up static resource delivery

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,14 +1,16 @@
 require('dotenv').config({ path: '../../../config/.env.server' });
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const router = require('./serverRouter');
 
 app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '../../frontend/tasks/')));
 app.use('/task/', router);
 
 app.get('/', (req, res) => {
-    res.send("Home Page.html");
+    res.sendFile(path.join(__dirname, '../../frontend/tasks/index.html'));
 })
 
 app.listen(process.env.SERVER_PORT, () => {
